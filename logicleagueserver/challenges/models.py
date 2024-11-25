a11f9e4c085734767a39d3f1c29e7328947cca3f
Ruddarm <ruddarmmaurya@gmail.com>
2024-11-25 23:27:57 +0530

    Added solution, and test case model

--- logicleagueserver/challenges/models.py
+++ logicleagueserver/challenges/models.py
@@ -3,6 +3,11 @@ from users.models import LogicLeagueUser
 import uuid
 
 # Create your models here.
+
+#Testcase 
+
+    
+#model Challenge model class
 class Challenges(models.Model):
     challengeID= models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name  = models.CharField(max_length=200,null=False)
@@ -19,5 +24,19 @@ class Challenges(models.Model):
     createdBy = models.ForeignKey(LogicLeagueUser,on_delete=models.CASCADE,related_name="Challenges")
     def __str__(self):
         return self.name
-class Solution:
-    pass
+class TestCase(models.Model):
+    testCaseID = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    ifnput = models.TextField(null=False)
+    output = models.TextField(null=False)
+    desc =  models.TextField(null=True)
+    challengeID = models.ForeignKey(Challenges,on_delete=models.CASCADE)    
+
+class Solution(models.Model):
+    solutionID= models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    language = models.CharField(max_length=100,null=False)
+    code = models.TextField(null=True,default="");
+    submission_date_time = models.DateTimeField
+    runtime = models.CharField(null=True)
+    space = models.CharField(null=True)
+    challengeID = models.ForeignKey(Challenges,on_delete=models.CASCADE)
+    submitedBy = models.ForeignKey(LogicLeagueUser,on_delete=models.CASCADE)
